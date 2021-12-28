4139c573a2afce6516097d10fe4d2be15a4dcb22
姬哀 <19585544+ji1ai1@users.noreply.github.com>
2021-12-29 02:18:24 +0800

    .

--- 基線/原版/預測器.h
+++ 基線/原版/預測器.h
@@ -17,6 +17,7 @@ namespace 番荔枝::特征編碼
 			std::vector<std::tuple<std::string, std::shared_ptr<std::vector<std::string>>>>& 預測向量
 			, const std::vector<std::shared_ptr<類別_樣本>>& 查詢樣本向量
 			, const std::vector<std::shared_ptr<類別_樣本>>& 畫廊樣本向量
+			, const std::vector<int>& 欄向量
 		)
 		{
 			const auto 執行緒數 = 28;
@@ -24,31 +25,31 @@ namespace 番荔枝::特征編碼
 			std::vector<std::tuple<std::string, std::shared_ptr<std::vector<std::string>>>> 預測向量陣列[執行緒數];
 			for (auto 子 = 0; 子 < 執行緒數; 子++)
 			{
-				執行緒陣列[子] = std::thread([查詢樣本向量, 畫廊樣本向量](auto 執行緒序号, auto 預測向量陣列)
-				{
-					for (auto 子 = size_t(執行緒序号); 子 < 查詢樣本向量.size(); 子 += 執行緒數)
+				執行緒陣列[子] = std::thread([查詢樣本向量, 畫廊樣本向量, 欄向量](auto 執行緒序号, auto 預測向量陣列)
 					{
-						if (子 % 1024 == 0)
-							std::cout << 取得時間() << "\t已預測" << 子 << "箇樣本......" << std::endl;
-
-						auto& 樣本 = *查詢樣本向量[子].get();
-
-						std::vector<std::tuple<std::string, double>> 檔案名相似度元組向量;
-						for(auto 丑 = 0; 丑 < 畫廊樣本向量.size(); 丑++)
+						for (auto 子 = size_t(執行緒序号); 子 < 查詢樣本向量.size(); 子 += 執行緒數)
 						{
-							auto& 畫廊樣本 = *畫廊樣本向量[丑].get();
-							auto 相似度 = 計算相似度(樣本, 畫廊樣本);
+							if (子 % 1024 == 0)
+								std::cout << 取得時間() << "\t已預測" << 子 << "箇樣本......" << std::endl;
 
-							檔案名相似度元組向量.push_back(std::tuple<std::string, double>(畫廊樣本.檔案名, 相似度));
+							auto& 樣本 = *查詢樣本向量[子].get();
+
+							std::vector<std::tuple<std::string, double>> 檔案名相似度元組向量;
+							for (auto 丑 = 0; 丑 < 畫廊樣本向量.size(); 丑++)
+							{
+								auto& 畫廊樣本 = *畫廊樣本向量[丑].get();
+								auto 相似度 = 計算相似度(樣本, 畫廊樣本, 欄向量);
+
+								檔案名相似度元組向量.push_back(std::tuple<std::string, double>(畫廊樣本.檔案名, 相似度));
+							}
+							std::sort(檔案名相似度元組向量.begin(), 檔案名相似度元組向量.end(), [](const auto& 子元組, const auto& 丑元組) { return std::get<1>(子元組) > std::get<1>(丑元組); });
+
+							auto 預測向量指針 = std::make_shared<std::vector<std::string>>();
+							for (auto 丑 = 0; 丑 < 100; 丑++)
+								預測向量指針->push_back(std::get<0>(檔案名相似度元組向量[丑]));
+							預測向量陣列[執行緒序号].push_back(std::tuple<std::string, std::shared_ptr<std::vector<std::string>>>(樣本.檔案名, 預測向量指針));
 						}
-						std::sort(檔案名相似度元組向量.begin(), 檔案名相似度元組向量.end(), [](const auto& 子元組, const auto& 丑元組) { return std::get<1>(子元組) > std::get<1>(丑元組); });
-
-						auto 預測向量指針 = std::make_shared<std::vector<std::string>>();
-						for (auto 丑 = 0; 丑 < 100; 丑++)
-							預測向量指針->push_back(std::get<0>(檔案名相似度元組向量[丑]));
-						預測向量陣列[執行緒序号].push_back(std::tuple<std::string, std::shared_ptr<std::vector<std::string>>>(樣本.檔案名, 預測向量指針));
-					}
-				}, 子, 預測向量陣列);
+					}, 子, 預測向量陣列);
 			}
 
 			for (auto 子 = 0; 子 < 執行緒數; 子++)
@@ -60,19 +61,19 @@ namespace 番荔枝::特征編碼
 		}
 
 	private:
-		static double 計算相似度(const 類別_樣本& 子樣本, const 類別_樣本& 丑樣本)
+		static double 計算相似度(const 類別_樣本& 子樣本, const 類別_樣本& 丑樣本, const std::vector<int>& 欄向量)
 		{
 			auto 相似度分子 = 0.0;
-			for (auto 子 = 0; 子 < 2048; 子++)
+			for (auto 子 : 欄向量)
 				相似度分子 += 子樣本.特征[子] * 丑樣本.特征[子];
 
 			return 相似度分子 / sqrt(子樣本.特征平方和 * 丑樣本.特征平方和);
 		}
 
-		//static double 計算相似度(const 類別_樣本& 子樣本, const 類別_樣本& 丑樣本)
+		//static double 計算相似度(const 類別_樣本& 子樣本, const 類別_樣本& 丑樣本, const std::vector<int>& 欄向量)
 		//{
 		//	auto 距離 = 0.0;
-		//	for (auto 子 = 0; 子 < 2048; 子++)
+		//	for (auto 子 : 欄向量)
 		//	{
 		//		auto 差 = 子樣本.特征[子] - 丑樣本.特征[子];
 		//		距離 += 差 * 差;
