04b27d43df11539f285b911d3ba40ce3203fad2a
姬哀 <19585544+ji1ai1@users.noreply.github.com>
2021-12-29 02:17:18 +0800

    .

--- 基線/简化字版/预测器.h
+++ 基線/简化字版/预测器.h
@@ -16,6 +16,7 @@ namespace 番荔枝::特征编码
 			std::vector<std::tuple<std::string, std::shared_ptr<std::vector<std::string>>>>& 预测向量
 			, const std::vector<std::shared_ptr<类别_样本>>& 查询样本向量
 			, const std::vector<std::shared_ptr<类别_样本>>& 画廊样本向量
+			, const std::vector<int>& 列向量
 		)
 		{
 			const auto 线程数 = 28;
@@ -23,7 +24,7 @@ namespace 番荔枝::特征编码
 			std::vector<std::tuple<std::string, std::shared_ptr<std::vector<std::string>>>> 预测向量数组[线程数];
 			for (auto 子 = 0; 子 < 线程数; 子++)
 			{
-				线程数组[子] = std::thread([查询样本向量, 画廊样本向量](auto 线程序号, auto 预测向量数组)
+				线程数组[子] = std::thread([查询样本向量, 画廊样本向量, 列向量](auto 线程序号, auto 预测向量数组)
 					{
 						for (auto 子 = size_t(线程序号); 子 < 查询样本向量.size(); 子 += 线程数)
 						{
@@ -36,7 +37,7 @@ namespace 番荔枝::特征编码
 							for (auto 丑 = 0; 丑 < 画廊样本向量.size(); 丑++)
 							{
 								auto& 画廊样本 = *画廊样本向量[丑].get();
-								auto 相似度 = 计算相似度(样本, 画廊样本);
+								auto 相似度 = 计算相似度(样本, 画廊样本, 列向量);
 
 								文件名相似度元组向量.push_back(std::tuple<std::string, double>(画廊样本.文件名, 相似度));
 							}
@@ -59,26 +60,24 @@ namespace 番荔枝::特征编码
 		}
 
 	private:
-		static double 计算相似度(const 类别_样本& 子样本, const 类别_样本& 丑样本)
+		static double 计算相似度(const 类别_样本& 子样本, const 类别_样本& 丑样本, const std::vector<int>& 列向量)
 		{
 			auto 相似度分子 = 0.0;
-			for (auto 子 = 0; 子 < 2048; 子++)
+			for (auto 子 : 列向量)
 				相似度分子 += 子样本.特征[子] * 丑样本.特征[子];
 
 			return 相似度分子 / sqrt(子样本.特征平方和 * 丑样本.特征平方和);
 		}
 
-		/*static double 计算相似度(const 类别_样本& 子样本, const 类别_样本& 丑样本)
+		/*static double 计算相似度(const 类别_样本& 子样本, const 类别_样本& 丑样本, const std::vector<int>& 列向量)
 		{
 			auto 距离 = 0.0;
-			for (auto 子 = 0; 子 < 2048; 子++)
+			for (auto 子 : 列向量)
 			{
 				auto 差 = 子样本.特征[子] - 丑样本.特征[子];
 				距离 += 差 * 差;
 			}
-
 			return 1 / (1 + 距离);
 		}*/
 	};
 }
-
