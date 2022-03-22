e62e7b1b648a079c4270f353803773b52e094b91
姬哀 <19585544+ji1ai1@users.noreply.github.com>
2022-03-22 13:36:32 +0800

    .

---
+++ 視覺特征編碼初賽第一階段/预测_简化字版.py
@@ -0,0 +1,18 @@
+import json
+import pandas
+
+# 预先用任意方式计算出余弦相似度，储存在「相似度.csv」档案中。每个query保留相似度最大的200个gallery即可。
+测试数据表 = pandas.read_csv("相似度.csv", header=None, names=["档案名", "预测档案名", "相似度"])
+测试数据表["排名"] = 测试数据表.groupby("filename")["相似度"].rank(ascending=False)
+测试数据表 = 测试数据表.loc[测试数据表.排名 <= 200]
+
+预测表 = 测试数据表.loc[:, ["档案名", "预测档案名"]].copy()
+预测表["相似度_16"] = 预测表.相似度 ** 16
+预测表 = 预测表.merge(预测表.groupby("预测档案名").aggregate(预测档案相似度_16和=("相似度_16", "sum")), on="预测档案名")
+预测表["打分"] = 预测表.相似度 / (1 + 预测表.预测档案相似度_16和 ** 0.03125) ** 4
+预测表["排名"] = 预测表.groupby("filename").打分.rank(ascending=False, method="first")
+预测表 = 预测表.loc[预测表.排名 <= 100].reset_index(drop=True)
+预测表 = 预测表.sort_values(["档案名", "排名"])
+with open("result.json", "w") as 档案:
+   json.dump(预测表.groupby("档案名").apply(lambda 子: list(子.预测档案名)).to_dict(), 档案)
+
